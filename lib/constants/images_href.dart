0abb66f0f6fb968a3e4d1e848170bc88e241b90d
Slupy <64200055+Slupy@users.noreply.github.com>
2022-09-04 20:38:48 +0300

    Update images_href.dart

--- lib/constants/images_href.dart
+++ lib/constants/images_href.dart
@@ -1,2 +1,3 @@
+// Изображение, если у трека нет обложки или он не доступен
 String noMusicImage =
     'https://community.spotify.com/t5/image/serverpage/image-id/55829iC2AD64ADB887E2A5/image-size/large?v=v2&px=999';
